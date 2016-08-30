23fe79a5eb0cf0b8eb5fe456bc5e13d4575ab31e
carole-lavillonniere <carole.lavillonniere@gmail.com>
2016-08-30 14:27:36 +0200

    Migration to Swift 3

--- Source/SwiftRouter.swift
+++ Source/SwiftRouter.swift
@@ -10,9 +10,9 @@ import Foundation
 import UIKit
 
 var appUrlSchemes:[String] = {
-    if let info:[String:AnyObject] = NSBundle.mainBundle().infoDictionary {
+    if let info:[String:AnyObject] = Bundle.main.infoDictionary as? [String: AnyObject] {
         var schemes = [String]()
-        if let url = info["CFBundleURLTypes"] as? [[String:AnyObject]]? where url != nil {
+        if let url = info["CFBundleURLTypes"] as? [[String:AnyObject]]? , url != nil {
             for d in url! {
                 if let scheme = d["CFBundleURLSchemes"]?[0] as? String{
                     schemes.append(scheme)
@@ -24,17 +24,17 @@ var appUrlSchemes:[String] = {
     return []
 }()
 
-enum RouterError:ErrorType {
-    case SchemeNotRecognized
-    case EntryAlreayExisted
-    case InvalidRouteEntry
+enum RouterError:Error {
+    case schemeNotRecognized
+    case entryAlreayExisted
+    case invalidRouteEntry
     func message() -> String {
         switch (self) {
-        case .SchemeNotRecognized:
+        case .schemeNotRecognized:
             return "SchemeNotRecognized"
-        case .EntryAlreayExisted:
+        case .entryAlreayExisted:
             return "EntryAlreayExisted"
-        case .InvalidRouteEntry:
+        case .invalidRouteEntry:
             return "InvalidRouteEntry"
         }
     }
@@ -45,14 +45,14 @@ class RouteEntry {
     var handler: (([String:String]?) -> Bool)? = nil
     var klass: AnyClass? = nil
     
-    init(pattern:String?, cls: AnyClass?=nil, handler:((params: [String:String]?) -> Bool)?=nil) {
+    init(pattern:String?, cls: AnyClass?=nil, handler:((_ params: [String:String]?) -> Bool)?=nil) {
         self.pattern = pattern
         self.klass = cls
         self.handler = handler
     }
 }
 
-extension RouteEntry: Swift.Printable, Swift.DebugPrintable {
+extension RouteEntry: Swift.CustomStringConvertible, Swift.CustomDebugStringConvertible {
     internal var description: String {
         let empty = ""
         if let k = self.klass {
@@ -61,7 +61,7 @@ extension RouteEntry: Swift.Printable, Swift.DebugPrintable {
         if let h = self.handler {
             return "\(self.pattern ?? empty) -> \(h)"
         }
-        fatalError(RouterError.InvalidRouteEntry.message())
+        fatalError(RouterError.invalidRouteEntry.message())
     }
     
     internal var debugDescription: String {
@@ -72,30 +72,30 @@ extension RouteEntry: Swift.Printable, Swift.DebugPrintable {
 extension String {
     func stringByFilterAppSchemes() -> String {
         for scheme in appUrlSchemes {
-            if self.hasPrefix(scheme.stringByAppendingString(":")) {
-                return self.substringFromIndex(self.startIndex.advancedBy((scheme.characters.count + 2)))
+            if self.hasPrefix(scheme + ":") {
+                return self.substring(from: self.characters.index(self.startIndex, offsetBy: (scheme.characters.count + 2)))
             }
         }
         return self
     }
 }
 
-public class Router {
-    public static let sharedInstance = Router()
+open class Router {
+    open static let sharedInstance = Router()
     
-    private let kRouteEntryKey = "_entry"
+    fileprivate let kRouteEntryKey = "_entry"
     
-    private var routeMap = NSMutableDictionary()
+    fileprivate var routeMap = NSMutableDictionary()
 
-    public func map(route: String, controllerClass: AnyClass) {
+    open func map(_ route: String, controllerClass: AnyClass) {
         self.doMap(route, cls: controllerClass)
     }
     
-    public func map(route: String, handler:([String:String]?) -> (Bool)) {
+    open func map(_ route: String, handler:(([String:String]?) -> (Bool))?) {
         self.doMap(route, handler: handler)
     }
     
-    internal func doMap(route: String, cls: AnyClass?=nil, handler:(([String:String]?) -> (Bool))?=nil) -> Void {
+    internal func doMap(_ route: String, cls: AnyClass?=nil, handler:(([String:String]?) -> (Bool))?=nil) -> Void {
         var r = RouteEntry(pattern: "/", cls: nil)
         if let k = cls {
             r = RouteEntry(pattern: route, cls: k)
@@ -106,10 +106,10 @@ public class Router {
         self.insertRoute(pathComponents, entry: r, subRoutes: self.routeMap)
     }
     
-    private func insertRoute(pathComponents: [String], entry: RouteEntry, subRoutes: NSMutableDictionary, index: Int = 0){
+    fileprivate func insertRoute(_ pathComponents: [String], entry: RouteEntry, subRoutes: NSMutableDictionary, index: Int = 0){
 
         if index >= pathComponents.count {
-            fatalError(RouterError.EntryAlreayExisted.message())
+            fatalError(RouterError.entryAlreayExisted.message())
         }
         let pathComponent = pathComponents[index]
         if subRoutes[pathComponent] == nil {
@@ -125,33 +125,33 @@ public class Router {
     }
     
     
-    public func matchController(route: String) -> AnyObject? {
+    open func matchController(_ route: String) -> AnyObject? {
         var params = self.paramsInRoute(route)
         if let entry = self.findRouteEntry(route, params: &params) {
             let name = NSStringFromClass(entry.klass!)
             let clz = NSClassFromString(name) as! NSObject.Type
             let instance = clz.init()
-            instance.setValuesForKeysWithDictionary(params)
+            instance.setValuesForKeys(params)
             return instance
         }
         return nil;
     }
     
-    public func matchControllerFromStoryboard(route: String, storyboardName: String = "Storyboard") -> AnyObject? {
+    open func matchControllerFromStoryboard(_ route: String, storyboardName: String = "Storyboard") -> AnyObject? {
         var params = self.paramsInRoute(route)
         if let entry = self.findRouteEntry(route, params: &params) {
             let name = NSStringFromClass(entry.klass!)
             let clz = NSClassFromString(name) as! NSObject.Type
-            let storyboard = UIStoryboard(name: storyboardName, bundle: NSBundle(forClass: clz))
-            let controllerIdentifier = name.componentsSeparatedByString(".").last!
-            let instance = storyboard.instantiateViewControllerWithIdentifier(controllerIdentifier)
-            instance.setValuesForKeysWithDictionary(params)
+            let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle(for: clz))
+            let controllerIdentifier = name.components(separatedBy: ".").last!
+            let instance = storyboard.instantiateViewController(withIdentifier: controllerIdentifier)
+            instance.setValuesForKeys(params)
             return instance
         }
         return nil;
     }
     
-    public func matchHandler(route: String) -> (([String:String]?) -> (Bool))? {
+    open func matchHandler(_ route: String) -> (([String:String]?) -> (Bool))? {
         var a = [String:String]()
         if let entry = self.findRouteEntry(route, params: &a) {
             return entry.handler
@@ -159,7 +159,7 @@ public class Router {
         return nil
     }
     
-    func findRouteEntry(route: String, inout params:[String:String]) -> RouteEntry? {
+    func findRouteEntry(_ route: String, params:inout [String:String]) -> RouteEntry? {
         let pathComponents = self.pathComponentsInRoute(route.stringByFilterAppSchemes())
         
         var subRoutes = self.routeMap
@@ -175,17 +175,16 @@ public class Router {
                     subRoutes = subRoutes[pathComponent] as! NSMutableDictionary
                     break
                 }
-                if k.hasPrefix(":") {
-                    let s = String(k)
-                    let key = s.substringFromIndex(s.startIndex.advancedBy(1))
+                if let s = k as? String, s.hasPrefix(":") {
+                    let key = s.substring(from: s.characters.index(s.startIndex, offsetBy: 1))
                     params[key] = pathComponent
-                    if pathComponent == pathComponents.last {
-                        return v[kRouteEntryKey] as? RouteEntry
+                    if let dict = v as? NSMutableDictionary, pathComponent == pathComponents.last {
+                        return dict[kRouteEntryKey] as? RouteEntry
                     }
                     subRoutes = subRoutes[s] as! NSMutableDictionary
                     break
                 } else {
-                    print(RouterError.SchemeNotRecognized.message())
+                    print(RouterError.schemeNotRecognized.message())
                     return nil
                 }
             }
@@ -193,22 +192,22 @@ public class Router {
         return nil
     }
     
-    func paramsInRoute(route: String) -> [String: String] {
+    func paramsInRoute(_ route: String) -> [String: String] {
 
         var params = [String:String]()
         self.findRouteEntry(route.stringByFilterAppSchemes(), params: &params)
 
         var path = route
 
-        if let loc = path.rangeOfString("#") {
-            for (key, value) in paramsFromQuery(path.substringFromIndex(loc.startIndex.advancedBy(1))) {
+        if let loc = path.range(of: "#") {
+            for (key, value) in paramsFromQuery(path.substring(from: path.index(after: loc.lowerBound))) {
                 params[key] = value
             }
-            path = path.substringToIndex(loc.startIndex)
+            path = path.substring(to: loc.lowerBound)
         }
 
-        if let loc = path.rangeOfString("?") {
-            for (key, value) in paramsFromQuery(path.substringFromIndex(loc.startIndex.advancedBy(1))) {
+        if let loc = path.range(of: "?") {
+            for (key, value) in paramsFromQuery(path.substring(from: path.index(after: loc.lowerBound))) {
                 params[key] = value
             }
         }
@@ -216,11 +215,11 @@ public class Router {
         return params
     }
 
-    private func paramsFromQuery(query: String) -> [String:String] {
+    fileprivate func paramsFromQuery(_ query: String) -> [String:String] {
         var params = [String:String]()
-        let paramArray = query.componentsSeparatedByString("&")
+        let paramArray = query.components(separatedBy: "&")
         for param in paramArray {
-            let kv = param.componentsSeparatedByString("=")
+            let kv = param.components(separatedBy: "=")
             if kv.count >= 2 {
                 let k = kv[0]
                 let v = kv[1]
@@ -230,16 +229,16 @@ public class Router {
         return params
     }
     
-    func pathComponentsInRoute(route: String) -> [String] {
+    func pathComponentsInRoute(_ route: String) -> [String] {
         var path:NSString = NSString(string: route)
-        if let loc = route.rangeOfString("#") {
-            path = NSString(string: route.substringToIndex(loc.startIndex))
+        if let loc = route.range(of: "#") {
+            path = NSString(string: route.substring(to: loc.lowerBound))
         }
-        if let loc = route.rangeOfString("?") {
-            path = NSString(string: route.substringToIndex(loc.startIndex))
+        if let loc = route.range(of: "?") {
+            path = NSString(string: route.substring(to: loc.lowerBound))
         }
         var result = [String]()
-        for (index, pathComponent) in path.pathComponents.enumerate() {
+        for (index, pathComponent) in path.pathComponents.enumerated() {
             if index > 0 && pathComponent == "/" { // don't ignore the leading `/` in order to support root matching
                 continue
             }
@@ -248,11 +247,11 @@ public class Router {
         return result
     }
     
-    public func removeAllRoutes() {
+    open func removeAllRoutes() {
         self.routeMap.removeAllObjects()
     }
     
-    public func routeURL(route:String) -> Bool {
+    open func routeURL(_ route:String) -> Bool {
         if let handler = self.matchHandler(route) {
             let params = self.paramsInRoute(route)
             return handler(params)
@@ -260,7 +259,7 @@ public class Router {
         return false
     }
 
-    public func routeURL(route:String, navigationController: UINavigationController) -> Bool {
+    open func routeURL(_ route:String, navigationController: UINavigationController) -> Bool {
         if let vc = self.matchController(route) {
             navigationController.pushViewController(vc as! UIViewController, animated: true)
             return true
